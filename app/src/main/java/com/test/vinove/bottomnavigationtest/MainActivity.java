2d623d38afad23560087a0e80894e714b5ee61ef
Parag <paragniist@gmail.com>
2018-04-17 16:27:19 +0530

    drawable changes

--- app/src/main/java/com/test/vinove/bottomnavigationtest/MainActivity.java
+++ app/src/main/java/com/test/vinove/bottomnavigationtest/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigation_id);
-        txt = (TextView) findViewById(R.id.text_id);
+        bottomNavigationView =  findViewById(R.id.bottomNavigation_id);
+        txt = findViewById(R.id.text_id);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
